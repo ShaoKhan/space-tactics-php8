8f5b496753cac97f959956ba96a4fe5659461968
ShaoKhan@gmx.de <ShaoKhan@gmx.de>
2023-03-28 19:13:44 +0200

    - database operations

--- src/Controller/BuildingsController.php
+++ src/Controller/BuildingsController.php
@@ -10,11 +10,10 @@
  *
  * You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace App\Controller;
 
-use App\Entity\Buildings;
 use App\Repository\BuildingsRepository;
 use App\Repository\PlanetRepository;
 use App\Repository\PlanetTypeRepository;
@@ -40,103 +39,37 @@ class BuildingsController extends AbstractController
     {
         $user_uuid = $security->getUser()->getUuid();
         $this->denyAccessUnlessGranted('ROLE_USER');
-        $planets = $this->getAllPlayerPlanets($managerRegistry, $user_uuid);
-        $firstPlanet = array_search($slug, array_column($planets, 'slug'));
+        $planets        = $this->getAllPlayerPlanets($managerRegistry, $user_uuid);
+        $firstPlanet    = array_search($slug, array_column($planets, 'slug'));
         $selectedPlanet = $planets[$firstPlanet];
 
-        if ($slug === NULL) {
+        if($slug === NULL) {
             $selectedPlanet = $planets[0];
         }
 
         $built = $p->getPlanetBuildings($user_uuid, $selectedPlanet, $managerRegistry);
-        $i=0;
-        foreach ($built as $building) {
-            $nextLevelProd = $bcs->calculateNextBuildingLevelProduction($building) * 3600;
-            $nextLevelBuildCost = $bcs->calculateNextBuildingCosts($building);
+        $i     = 0;
+        foreach($built as $building) {
+            $nextLevelProd       = $bcs->calculateNextBuildingLevelProduction($building) * 3600;
+            $nextLevelBuildCost  = $bcs->calculateNextBuildingCosts($building);
             $nextLevelEnergyCost = $bcs->calculateNextBuildingLevelEnergyCosts($building) * 3600;
 
 
-
-            $built[$i]['production'] = number_format($nextLevelProd,0,',','.');
-            $built[$i]['nextEnergyCosts'] = number_format($nextLevelEnergyCost,0,',','.');
-            $built[$i]['BuildCosts'] = $nextLevelBuildCost;
+            $built[$i]['production']      = number_format($nextLevelProd, 0, ',', '.');
+            $built[$i]['nextEnergyCosts'] = number_format($nextLevelEnergyCost, 0, ',', '.');
+            $built[$i]['BuildCosts']      = $nextLevelBuildCost;
             $i++;
         }
 
-        return $this->render('buildings/index.html.twig', [
-            'planets' => $planets,
+        return $this->render(
+            'buildings/index.html.twig', [
+            'planets'        => $planets,
             'selectedPlanet' => $selectedPlanet,
-            'user' => $this->getUser(),
-            'slug' => $slug,
-            'buildings' => $built ?? NULL,
-        ]);
-    }
-
-    public function getBuildingProduction(string $buildingName, int $level, array $factor, float $planetTemp): array
-    {
-        switch ($buildingName) {
-            case 'metal_building':
-                $prod = (30 * $level * pow((1.1), $level)) * (0.1 * $factor[0]->getFactor());
-                $costEnergy = -(10 * $level * pow((1.1), $level)) * (0.1 * $factor[0]->getFactor());
-                break;
-            case 'crystal_building':
-                $prod = (20 * $level * pow((1.1), $level)) * (0.1 * $factor[0]->getFactor());
-                $costEnergy = -(10 * $level * pow((1.1), $level)) * (0.1 * $factor[0]->getFactor());
-                break;
-            case 'deuterium_building':
-                $prod = (10 * $level * pow((1.1), $level) * (-0.002 * $planetTemp + 1.28) * (0.1 * $factor[0]->getFactor()));
-                $costEnergy = -(30 * $level * pow((1.1), $level)) * (0.1 * $factor[0]->getFactor());
-                break;
-            default:
-                $prod = 0.0;
-                $costEnergy = 0.0;
-                break;
-        }
-        return ['production' => $prod, 'costEnergy' => $costEnergy];
-    }
-
-    public function getMetalCosts($factor, $level)
-    {
-        return $factor[0]->getCostMetal() * pow($factor[0]->getFactor(), $level);
-    }
-
-    public function getCrystalCosts($factor, $level)
-    {
-        return $factor[0]->getCostCrystal() * pow($factor[0]->getFactor(), $level);
-    }
-
-    public function getDeuteriumCosts($factor, $level)
-    {
-        return $factor[0]->getCostDeuterium() * pow($factor[0]->getFactor(), $level);
-    }
-
-    private function BuildingMapping(): array
-    {
-
-        $buildingMap = [
-            1 => 'metal_building',
-            2 => 'crystal_building',
-            3 => 'deuterium_building',
-            4 => 'solar_building',
-            5 => 'university_building',
-            6 => 'nuclear_building',
-            7 => 'robot_building',
-            8 => 'nanite_building',
-            9 => 'hangar_building',
-            10 => 'metalstorage_building',
-            11 => 'crystalstorage_building',
-            12 => 'deuteriumstorage_building',
-            13 => 'laboratory_building',
-            14 => 'terraforming_building',
-            15 => 'alliancehangar_building',
-            16 => 'moonbase_building',
-            17 => 'missilesilo_building',
-            18 => 'jumpgate_building',
-            19 => 'laserphalanx_building',
-
-        ];
-
-        return $buildingMap;
+            'user'           => $this->getUser(),
+            'slug'           => $slug,
+            'buildings'      => $built ?? NULL,
+        ],
+        );
     }
 
 }
--- src/Service/BuildingCalculationService.php
+++ src/Service/BuildingCalculationService.php
@@ -8,27 +8,27 @@ class BuildingCalculationService
     public function calculateNextBuildingLevelProduction(array $building): int
     {
         return match ($building["id"]) {
-            1 => (30 * ($building["building_level"] + 1) * pow((1.1), ($building["building_level"] + 1))) * (0.1 * $building["factor"]),
-            2, 4 => (20 * ($building["building_level"] + 1) * pow((1.1), ($building["building_level"]+ 1))) * (0.1 * $building["factor"]),
-            3 => (10 * ($building["building_level"] + 1) * pow((1.1), ($building["building_level"] + 1))) * (0.1 * $building["factor"]),
+            1       => (30 * ($building["building_level"] + 1) * pow((1.1), ($building["building_level"] + 1))) * (0.1 * $building["factor"]),
+            2, 4    => (20 * ($building["building_level"] + 1) * pow((1.1), ($building["building_level"] + 1))) * (0.1 * $building["factor"]),
+            3       => (10 * ($building["building_level"] + 1) * pow((1.1), ($building["building_level"] + 1))) * (0.1 * $building["factor"]),
             default => 0,
         };
     }
 
-    public function calculateNextBuildingLevelEnergyCosts(array $building):int
+    public function calculateNextBuildingLevelEnergyCosts(array $building): int
     {
         return match ($building["id"]) {
-            1, 2 => -(10 * ($building["building_level"] + 1) * pow((1.1), $building["building_level"])) * (0.1 * $building["factor"]),
-            3 => -(30 * $building["building_level"] * pow((1.1), $building["building_level"])) * (0.1 * $building["factor"]),
+            1, 2    => -(10 * ($building["building_level"] + 1) * pow((1.1), $building["building_level"])) * (0.1 * $building["factor"]),
+            3       => -(30 * $building["building_level"] * pow((1.1), $building["building_level"])) * (0.1 * $building["factor"]),
             default => 0,
         };
     }
 
-    public function calculateNextBuildingCosts(array $building):array
+    public function calculateNextBuildingCosts(array $building): array
     {
-        $buildCosts["metal"] = ceil($building["cost_metal"] * pow($building["factor"], $building["building_level"] + 1));
-        $buildCosts["crystal"] = ceil($building["cost_crystal"] * pow($building["factor"], $building["building_level"] + 1));
-        $buildCosts["deuterium"] = ceil($building["cost_deuterium"] * pow($building["factor"], $building["building_level"] + 1));
+        $buildCosts["metal"]       = ceil($building["cost_metal"] * pow($building["factor"], $building["building_level"] + 1));
+        $buildCosts["crystal"]     = ceil($building["cost_crystal"] * pow($building["factor"], $building["building_level"] + 1));
+        $buildCosts["deuterium"]   = ceil($building["cost_deuterium"] * pow($building["factor"], $building["building_level"] + 1));
         $buildCosts["dark_matter"] = ceil($building["cost_dark_matter"] * pow($building["factor"], $building["building_level"] + 1));
         return $buildCosts;
 
