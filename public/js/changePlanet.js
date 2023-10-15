f416a4fce82ab5b14ae64bde354376fa9914dda7
Silvio <Silvio.Habel@gmail.com>
2023-10-15 23:26:59 +0200

    resolved codeql hint

--- public/js/changePlanet.js
+++ public/js/changePlanet.js
@@ -2,7 +2,8 @@ $(document).ready(function () {
     $('.planet-switcher-select-main, .planet-switcher-select').on('change', function () {
         const selectedOption = $(this).children('option:selected').val();
         const referer = window.location.pathname.split('/')[1] || 'main';
-        window.location.href = `/${referer}/${selectedOption}`;
+        const sanitizedSelectedOption = encodeURIComponent(selectedOption); // Sanitize the selected option
+        const sanitizedReferer = encodeURIComponent(referer); // Sanitize the referer
+        window.location.href = `/${sanitizedReferer}/${sanitizedSelectedOption}`;
     });
 });
-
